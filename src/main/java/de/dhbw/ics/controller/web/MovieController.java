918af6801c08403da40407f867891eda245a1c5a
Felix Waage <felix.waage@sap.com>
2019-01-10 10:32:18 +0100

    Movie endpoint mit Fehlern

--- src/main/java/de/dhbw/ics/controller/web/MovieController.java
+++ src/main/java/de/dhbw/ics/controller/web/MovieController.java
@@ -1,25 +1,35 @@
 package de.dhbw.ics.controller.web;
 
+import de.dhbw.ics.database.dao.GenreDao;
+import de.dhbw.ics.database.dao.MovieDao;
+import de.dhbw.ics.spring.ApplicationContextProvider;
 import de.dhbw.ics.vo.Genre;
 import de.dhbw.ics.vo.Movie;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class MovieController {
 
+    private MovieDao movieDao = ApplicationContextProvider.getContext().getBean("movieDao", MovieDao.class);
+    private GenreDao genreDao = ApplicationContextProvider.getContext().getBean("genreDao", GenreDao.class);
 
-    @RequestMapping(method=RequestMethod.GET, path = "/get/movie")
+    @RequestMapping(method=RequestMethod.GET, path = "/get/movies")
     public @ResponseBody
-    ResponseEntity<Movie> getMovie() {
-        Movie movie = new Movie(2015, "TestMovie", "Nice Test Movie", 12, 120);
-        Genre genre = new Genre("testGenre");
-        movie.setGenre(genre);
-        return new ResponseEntity<>(movie, HttpStatus.OK);
+    ResponseEntity<List<Movie>> getMovies() {
+        List<Movie> movieList = movieDao.getAll();
+
+        return new ResponseEntity<>(movieList, HttpStatus.OK);
     }
 
+    @PostMapping("/post/movie")
+    public void setMovie(@RequestBody Movie bodyMovie){
+
+        Movie newMovie = new Movie(bodyMovie.getProductionYear(),bodyMovie.getTitle(),bodyMovie.getDescription(),bodyMovie.getFsk(), bodyMovie.getRunTime(), bodyMovie.getGenre());
+        movieDao.persist(newMovie);
+
+    }
 }
