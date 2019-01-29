ea44f3e8a1b76e7fd5c04d279a2b2ba73245ee75
Martin <martinf95@web.de>
2019-01-29 11:00:25 +0100

    fix cors

--- src/main/java/de/dhbw/ics/spring/IcsWebConfiguration.java
+++ src/main/java/de/dhbw/ics/spring/IcsWebConfiguration.java
@@ -6,13 +6,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
-;
 
 @Configuration
 @ImportResource("classpath:spring/applicationContext.xml")
@@ -48,19 +48,9 @@ public class IcsWebConfiguration implements WebMvcConfigurer {
         }
     }
 
-
-/*    @Bean
-    public MappingJackson2HttpMessageConverter customJackson2HttpMessageConverter() {
-        MappingJackson2HttpMessageConverter jsonConverter = new MappingJackson2HttpMessageConverter();
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        jsonConverter.setObjectMapper(objectMapper);
-        return jsonConverter;
-    }
-
     @Override
-    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-        converters.add(customJackson2HttpMessageConverter());
-    }*/
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
 
 }
